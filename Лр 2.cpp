8d45c86c75f49a0040550bee1083870a61f27950
Nikast147 <nikastpro228@gmail.com>
2024-10-27 17:57:14 +0200

    Add LR2

---
+++ Лр 2.cpp
@@ -0,0 +1,26 @@
+﻿
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+ // Завдання 1 
+	cout << setw(10) << right << showpos << 4 << endl;
+	cout << setw(15) << setprecision(4) << right << internal << -67.09124 << endl;
+	cout << setw(10) << right << showpos << 235 << endl;
+	cout << hex << 2209 << endl;
+	cout << fixed << setprecision(2) << -121.0 << endl;
+	cout << dec << showpos << 1 << 24 << endl;
+ // Завдання 2
+	cout << "String1" << '\n' << setw(15) << right << "String2" << '\n' << setw(23) << right << "String3" << endl;
+	cout << setw(15) << right << "String1" << '\n' << "String2" << endl;
+	cout << "3";
+	cout << '*';
+	cout << setw(12) << right << "String" << '\n' << '1';
+
+
+}
+
+
